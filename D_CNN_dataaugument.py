25016e8ded5f98a5dd7da64d2c46e78bdcbf282f
Bansari Thummar <bansarithummar@gmail.com>
2025-03-13 16:40:07 -0500

    Update D_CNN_dataaugument.py

--- D_CNN_dataaugument.py
+++ D_CNN_dataaugument.py
@@ -6,7 +6,6 @@ import numpy as np
 EPOCHS=50
 NUM_CLASSES = 10
     
-
 def load_data():
     (x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
     x_train = x_train.astype('float32')
